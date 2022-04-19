a44e458b2d6e026e9df7e72602a01632a584fe39
srinu <srinu@192.168.0.109>
2022-04-19 11:25:39 +0530

    my first commit

---
+++ src/test/java/Tests/Testdummy.java
@@ -0,0 +1,13 @@
+package Tests;
+
+public class Testdummy 
+{
+	public static void main(String[] args)
+	{
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println(z);
+		System.out.println(x-y);
+	}
+}
